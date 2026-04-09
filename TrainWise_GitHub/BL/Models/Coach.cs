14543454d2e7f150f8299a2b77f8580e0fc5692c
Lirone Ouak <lironeouak@gmail.com>
2026-04-10 01:34:01 +0300

    Delete TrainWise_GitHub directory

--- TrainWise_GitHub/BL/Models/Coach.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrainWise.BL.Models
-{
-    public class Coach
-    {
-        public int CoachID { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-    }
-}
