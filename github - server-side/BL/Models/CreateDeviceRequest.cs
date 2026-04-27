5b733db989def7134fd4ba8b70f27b17fc6a8798
Lirone Ouak <lironeouak@gmail.com>
2026-04-27 12:05:36 +0300

    Delete github - server-side directory

--- github - server-side/BL/Models/CreateDeviceRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrainWise.BL.Models
-{
-    public class CreateDeviceRequest
-    {
-        public string DeviceName { get; set; }
-        public DateTime LastSync { get; set; }
-        public bool PermissionsGranted { get; set; }
-    }
-}
--- github - server-side/BL/Models/InjuryType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TrainWise.BL.Models
-{
-    public class InjuryType
-    {
-        public int InjuryTypeID { get; set; }
-        public string InjuryName { get; set; }
-    }
-}
